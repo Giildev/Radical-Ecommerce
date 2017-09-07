3dbbcd3781889c1afe99b9268fcdba82d13b2a79
Gilberto Marcano <gilberto.marcano.dev@gmail.com>
2017-09-07 04:18:51 -0400

    Basic structure

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PageHeader } from 'react-bootstrap';
+import './style.css';
+
+const Header = () => (
+	<div>
+		<PageHeader className="header">Example Header</PageHeader>
+	</div>
+);
+
+export default Header;
