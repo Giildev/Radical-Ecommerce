2fb4dabb83a0425d4b637b0def9a4a2bf15b5d40
Gilberto Marcano <gilberto.marcano.dev@gmail.com>
2018-04-05 05:19:22 -0400

    Google Map

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,18 +1,18 @@
 // Dependencies
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Gmap from "../../components/Gmap/";
 // Components & Containers
-import './style.css';
+import "./style.css";
 
 class Home extends Component {
-	
-	render() {
-		return (
-			<div>
-				<p>Work in progresss</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <p>Work in progresss</p>
+        <Gmap />
+      </div>
+    );
+  }
 }
 
 export default Home;
