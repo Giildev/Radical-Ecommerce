be65abb0f1ed15c9bf7d8533edf6f98e6e81c598
Gilberto Marcano <gilberto.marcano.dev@gmail.com>
2018-04-14 03:22:00 -0400

    /add-to-cart: Auto stash before merge of "feature/add-to-cart" and "feature/contact-us"

---
+++ src/components/AddToCart/index.jsx
@@ -0,0 +1,13 @@
+// Dependencies
+import React from "react";
+
+// Components & Containers
+import "./style.css";
+
+const AddToCart = () => (
+  <div>
+    <h1>Code</h1>
+  </div>
+);
+
+export default AddToCart;
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,17 +1,20 @@
 // Dependencies
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import AddToCart from '../../components/AddToCart/'
 
 // Components & Containers
 import "./style.css";
 
 class Home extends Component {
-  render() {
-    return (
-      <div>
-        <p>Work in progresss</p>
-      </div>
-    );
-  }
+	
+	render() {
+		return (
+			<div>
+				<p>Work in progresss</p>
+				<AddToCart />
+			</div>
+		);
+	}
 }
 
 export default Home;
