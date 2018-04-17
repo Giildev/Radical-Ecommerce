366d7c68d43ded694e9c67f4ec2551ca33480703
Andres Rodriguez <zplaguee@gmail.com>
2018-04-16 21:59:06 -0400

    Header full

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,42 @@
 // Dependencies
 import React from "react";
+import Logo from "../../assets/img/logo-web.png"
+import {Sprite} from "../../assets/img/sprites.svg"
 
 // Components & Containers
 import "./style.css";
 
 const Header = () => (
-  <div>
-    <h1>GILBERTICO</h1>
-  </div>
+  <header className="headerContainer">
+    <img src={Logo} alt="Logo Radical" className="logo"/>
+
+    <nav className="menu">
+      <ul className="menu__list">
+        <li className="menu__list__item">Tienda</li>
+        <li className="menu__list__item">Ofertas</li>
+        <li className="menu__list__item">Nuevo</li>
+        <li className="menu__list__item">Contacto</li>
+      </ul>
+    </nav>
+
+    <div className="userMeta">
+      <button className="cart">
+        <svg className="cart__icon">
+          <use xlinkHref="../../assets/img/sprites.svg#icon-shopping-cart"></use>
+        </svg>
+        <span className="cart__notification">5</span>
+      </button>
+  
+      <button className="btnRound btnRound--red">Registrarse</button>
+    
+      <button className="btnRound btnRound--black">Iniciar Sesión</button>
+    
+    </div>
+
+
+    
+    
+  </header>
 );
 
 export default Header;
