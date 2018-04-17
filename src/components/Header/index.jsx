7451e50608302e25e6bfdfe30e516a4ba19e316c
Gilberto Marcano <gilberto.marcano.dev@gmail.com>
2018-04-17 19:10:49 -0400

    fixs

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,23 @@
 // Dependencies
 import React from "react";
-import Logo from "../../assets/img/logo-web.png"
-import {Sprite} from "../../assets/img/sprites.svg"
+import Logo from "../../assets/img/logo-web.png";
 
 // Components & Containers
 import "./style.css";
 
 const Header = () => (
   <header className="headerContainer">
-    <img src={Logo} alt="Logo Radical" className="logo"/>
+    <img src={Logo} alt="Logo Radical" className="logo" />
 
     <nav className="menu">
       <ul className="menu__list">
-        <li className="menu__list__item__dropdown">Tienda
+        <li className="menu__list__item__dropdown">
+          Tienda
           <ul className="dropdown">
             <li className="dropdown__item">Caballeros</li>
             <li className="dropdown__item">Damas</li>
             <li className="dropdown__item">Niños</li>
-          </ul> 
+          </ul>
         </li>
 
         <li className="menu__list__item">Ofertas</li>
@@ -28,21 +28,14 @@ const Header = () => (
 
     <div className="userMeta">
       <div className="cart">
-        <div className="cart__icon">
-         <a href="#"><img href="../../assets/img/ICON/cart.png"></img></a> 
-        </div>
+        <div className="cart__icon"/>
         <span className="cart__notification">5</span>
       </div>
-  
+
       <button className="btnRound btnRound--red">Registrarse</button>
-    
+
       <button className="btnRound btnRound--black">Iniciar Sesión</button>
-    
     </div>
-
-
-    
-    
   </header>
 );
 
