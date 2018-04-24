0d079ae18fe98ded46e44f43bf8cdbfd4cf44f96
Andres Rodriguez <zplaguee@gmail.com>
2018-04-24 02:26:33 -0400

    Discover Creation

---
+++ client/src/containers/Discover/index.jsx
@@ -0,0 +1,17 @@
+// Dependencies
+import React, { Component } from "react";
+
+// Components & Containers
+import "./style.css";
+
+class Container extends Component {
+  render() {
+    return (
+      <div>
+        <p>Code!</p>
+      </div>
+    );
+  }
+}
+
+export default Container;
